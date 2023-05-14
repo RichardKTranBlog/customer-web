d17b8f1f3a7e7f4eebb79ce02c7189bd6643e084
richardktran <richardktran.dev@gmail.com>
2023-05-15 01:35:57 +0700

    update time to 8 to test infrastructure

--- src/components/blog/BlogItem.tsx
+++ src/components/blog/BlogItem.tsx
@@ -76,7 +76,7 @@ export const BlogItem = ({ href, ...props }: BlogItemProps) => {
       </Text>
       <Flex justify="space-between">
         <Text color={subTextColor} fontSize="sm">
-          7 April, 2023
+          8 April, 2023
         </Text>
         <Link href={href}>
           <Flex align="center">
