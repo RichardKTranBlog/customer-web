897d30255a0f093057f7c41ac8895b5513225ca1
richardktran <richardktran.dev@gmail.com>
2023-05-15 22:47:16 +0700

    test hook jenkins

--- src/components/blog/BlogItem.tsx
+++ src/components/blog/BlogItem.tsx
@@ -76,7 +76,7 @@ export const BlogItem = ({ href, ...props }: BlogItemProps) => {
       </Text>
       <Flex justify="space-between">
         <Text color={subTextColor} fontSize="sm">
-          10 April, 2023
+          11 April, 2023
         </Text>
         <Link href={href}>
           <Flex align="center">
